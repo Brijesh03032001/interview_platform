2feb24334955160597ec86d433f2616b4a896fb8
Brijesh03032001 <sainibrijesh01@gmail.com>
2025-06-03 20:56:12 -0700

    interview platform all functionalities

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+// https://excited-snail-7.clerk.accounts.dev
+
+export default {
+    providers: [
+      {
+        domain: "https://excited-snail-7.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
